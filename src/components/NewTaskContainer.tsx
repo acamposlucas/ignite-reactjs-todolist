1945e10dcd551e08132bf309f0f14cc9043f29a0
Lucas <lac.lucas96@gmail.com>
2022-06-06 15:03:19 -0300

    feat: create NewTask button

--- src/components/NewTaskContainer.tsx
+++ src/components/NewTaskContainer.tsx
@@ -2,9 +2,16 @@ import styles from "./NewTaskContainer.module.css";
 
 export function NewTaskContainer() {
   return (
-    <div className={styles.input__container}>
-      <label htmlFor="newTask"></label>
-      <input type="text" id="newTask" placeholder="Adicione uma nova tarefa" />
+    <div className={styles.wrapper}>
+      <div className={styles.input__container}>
+        <label htmlFor="newTask"></label>
+        <input
+          type="text"
+          id="newTask"
+          placeholder="Adicione uma nova tarefa"
+        />
+      </div>
+      <button>Criar</button>
     </div>
   );
 }
